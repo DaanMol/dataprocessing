bf093b2eb689fc851802b98629b922498e030da6
DaanMol <Daan.molleman@Gmail.com>
2018-11-09 13:30:41 +0100

    fully working eda.py with correct json file :)

--- Homework/week_2/eda.py
+++ Homework/week_2/eda.py
@@ -2,68 +2,74 @@
 # Name: Daan Molleman
 # Student number: 11275820
 """
-This script...
+This script parses a csv file of countries and their statistics.
+It then creates a Pandas DataFrame of this data and calculates
+stats for the gdp and creates a histogram. Then it creates a
+boxplot of the infant mortality rate and gives its stats.
+Lastly it converts the parsed data into a JSON file
 """
 
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import json as j
 
 INPUT = 'input.csv'
+# the highgest gdp per capita is that of Luxembourg
+GDPMAX = 107708.22
+
 
 def parse(reader):
     """
     Collects data from a csv file
     """
 
-    # get needed data from file
-    country_dict = {}
-    # countries = []
-    # regions = []
-    # pop_dens = []
-    # inf_mort = []
-    # gdp = []
+    # create dictionary to fill with countries and their stats
+    data = {}
+
+    # read the file, if a country has missing or invalid data,
+    # do not include the country in the dictionary
     for row in reader:
-        # countries.append(row['Country'])
         country = row['Country'].strip(' ')
-        # regions.append(row['Region'].strip(' '))
         region = row['Region'].strip(' ')
 
         pop = row['Pop. Density (per sq. mi.)']
-        # print(pop)
         if pop == 'unknown':
-            # pop_dens.append(0)
-            pop = 0
+            continue
         else:
-            # pop_dens.append(float(pop.replace(',', '.')))
             pop = float(pop.replace(',', '.'))
 
         mort = row['Infant mortality (per 1000 births)']
         if mort:
-            # inf_mort.append(float(mort.replace(',', '.')))
             mort = float(mort.replace(',', '.'))
         else:
-            # inf_mort.append(0)
-            mort = 0
+            continue
 
-        curr_gdp = row['GDP ($ per capita) dollars']
-        if curr_gdp == 'unknown' or curr_gdp == '' or int(curr_gdp.strip(' dollars')) > 399999:
-            # gdp.append(0)
-            curr_gdp = 0
+        gdp = row['GDP ($ per capita) dollars']
+        if gdp == 'unknown' or gdp == '' \
+           or int(gdp.strip(' dollars')) > GDPMAX:
+            continue
         else:
-            # gdp.append(int(curr_gdp.strip(' dollars')))
-            curr_gdp = int(curr_gdp.strip(' dollars'))
+            gdp = int(gdp.strip(' dollars'))
 
-        country_dict[country] = [region, pop, mort, curr_gdp]
-        # print(country_dict)
+        # create dictionary input for each country
+        data[country] = {'Region': region, "Pop. Density (per sq. mi.)": pop,
+                         "Infant mortality (per 1000 births)": mort,
+                         "GDP ($ per capita) dollars": gdp}
 
-    # return[countries, regions, pop_dens, inf_mort, gdp]
-    return country_dict
+    return data
+
 
 def histo(frame, column):
     """
     Plots a histogram for the specified column
     """
+    # print the stats for the GDP per capita
+    print(column, 'stats:')
+    print("Mean =", frame[column].mean())
+    print("Median =", frame[column].median())
+    print("Mode =", frame[column].mode())
+
     gdplist = []
     for product in frame[column]:
         gdplist.append(product)
@@ -77,16 +83,35 @@ def histo(frame, column):
 def boxplot(frame, column):
     """
     Plots a boxplot of the given column
+    and prints the statistics
     """
     values = []
     for value in frame[column]:
         values.append(value)
-    numbers = pd.Series(values)
-    stats = numbers.describe()
+
+    # create a series of the values in the column to make it
+    # suitabel for describe function
+    stats = pd.Series(values).describe()
+    median = frame[column].median()
+
+    print(column, 'stats:')
     print('Min =', stats["min"])
-    print('Mean =', stats['mean'])
+    print('25% =', stats["25%"])
+    print('Median =', median)
+    print('75% = ', stats['75%'])
+    print('Max = ', stats['max'])
     plt.boxplot(values)
     plt.show()
+
+
+def json(parsed):
+    """
+    Converts a dictionary to a json file
+    """
+
+    with open("output.json", "w") as write_file:
+        j.dump(parsed, write_file, indent=4)
+
 
 if __name__ == '__main__':
 
@@ -96,14 +121,12 @@ if __name__ == '__main__':
         parsed = parse(reader)
 
     # create dataframe of parsed data
-    frame = pd.DataFrame(parsed, index=['Region', 'Population density',
-                                        'Infant mortality rate', 'GDP'])
+    frame = pd.DataFrame(parsed)
+
     # flip the rows and columns
     frame = frame.transpose()
 
-    print("Mean =", frame['GDP'].mean())
-    print("Median =", frame['GDP'].median())
-    print("Mode =", frame['GDP'].mode())
-
-    # histo(frame, 'Infant mortality rate')
-    boxplot(frame, 'Infant mortality rate')
+    # create a histogram, a boxplot and a json file
+    histo(frame, 'GDP ($ per capita) dollars')
+    boxplot(frame, 'Infant mortality (per 1000 births)')
+    json(parsed)
