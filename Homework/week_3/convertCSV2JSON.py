99a624e93b7b4a4d10718bb7e2761e1a43b3e2c6
DaanMol <Daan.molleman@Gmail.com>
2018-11-15 14:01:31 +0100

    working parser and json converter

---
+++ Homework/week_3/convertCSV2JSON.py
@@ -0,0 +1,41 @@
+# Daan Molleman
+# 11275820
+
+"""
+This script converts a csv file to a JSON file
+"""
+
+import json as j
+import csv
+
+INPUT = "england.csv"
+
+def parse(reader):
+    """
+    parses a csv file into a dict
+    """
+
+    all_data = {}
+
+    for row in reader:
+        data = {}
+        for i in row:
+            data[i] = row[i]
+        all_data[row['Area']] = data
+
+    return all_data
+
+def json(parsed):
+    """
+    Converts a dictionary to a json file
+    """
+
+    with open("output.json", "w") as write_file:
+        j.dump(parsed, write_file, indent=4)
+
+if __name__ == '__main__':
+    with open(INPUT) as file:
+        reader = csv.DictReader(file)
+        parsed = parse(reader)
+
+        json(parsed)
