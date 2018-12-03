1e645abc4c70b243430596bbfa851a212eda966c
DaanMol <Daan.molleman@Gmail.com>
2018-12-03 13:09:20 +0100

    per land dots. inconsequente data.

--- Homework/week_5/scatter.js
+++ Homework/week_5/scatter.js
@@ -10,12 +10,6 @@ var consConf = "http://stats.oecd.org/SDMX-JSON/data/HH_DASH/FRA+DEU+KOR+NLD+PRT
 
 var requests = [d3.json(womenInScience), d3.json(consConf)];
 
-// define the countries that are not explicitly defined in the dataset
-const countries = ["France", "Netherlands", "Portugal", "Germany",
-                 "United Kingdom", "Korea"];
-// const years = ["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014",
-//                "2015"];
-
 // define width and height of the svg
 const w = 1000;
 const h = 450;
@@ -37,18 +31,30 @@ d3.select("body").append("h4").text("source: OECD (2018)")
 window.onload = function() {
   // request api
   Promise.all(requests).then(function(response) {
+    console.log(response)
       let womenValues = parseData(response[0]);
       let consValues = parseData(response[1]);
 
-      dots = makeDots(womenValues, consValues);
+      userInput = "Portugal"
+      console.log(womenValues)
+      console.log(consValues)
+      // console.log(countries.indexOf(userInput))
+      // console.log("yes")
+      userSelection1 = womenValues[countries1.indexOf(userInput)][userInput]
+      userSelection2 = consValues[countries2.indexOf(userInput)][userInput]
+      // console.log(userSelection1)
+      // console.log(userSelection2)
+
+      dots = makeDots(userSelection1, userSelection2);
 
       // create scales
       var xScale = d3.scaleLinear()
-                     .domain([calc(womenValues, "min"),
-                              calc(womenValues, "max")])
+                     .domain([calc(userSelection1, "min") - 1,
+                              calc(userSelection1, "max") + 1])
                      .range([xPadding, w - padding])
       var yScale = d3.scaleLinear()
-                     .domain([calc(consValues, "min"), calc(consValues, "max")])
+                     .domain([calc(userSelection2, "min") - 1,
+                              calc(userSelection2, "max") + 1])
                      .range([h-padding, padding]);
 
       // initialise svg variable
@@ -62,20 +68,44 @@ window.onload = function() {
          .data(dots)
          .enter()
          .append("circle")
-         .attr("cx", function(d) {
-           console.log(d.women)
-           return xScale(d.women);
+         .attr("cx", function(d, i) {
+             return xScale(d[Object.keys(d)][0]);
          })
-         .attr("cy", function(d) {
-             return yScale(d.cons);
+         .attr("cy", function(d, i) {
+             return yScale(d[Object.keys(d)][1]);
          })
-         .attr("r", 5);
+         .attr("r", 5)
+         .attr("class", "normal");
+
+      // insert circle tags
+      svg.selectAll("text")
+         .data(dots)
+         .enter()
+         .append("text")
+         .text(function(d) {
+            return Object.keys(d)[0];
+         })
+         .attr("x", function(d) {
+            return xScale(d[Object.keys(d)][0]) + 10;
+         })
+         .attr("y", function(d) {
+            return yScale(d[Object.keys(d)][1]);
+         })
 
       // call x-axis ticks
       svg.append("g")
          .attr("class", "axis")
          .attr("transform", "translate(0," + (h - padding) + ")")
          .call(d3.axisBottom(xScale));
+
+      // create y axis label
+      svg.append("text")
+         .attr("y", h - 10)
+         .attr("x", w / 2)
+         .attr("dy", "1em")
+         .style("text-anchor", "middle")
+         .style("font-family", "Monospace")
+         .text("Percentage of women in science");
 
       // call y-axis ticks
       svg.append("g")
@@ -96,33 +126,63 @@ window.onload = function() {
 }
 
 function parseData(data) {
-    // the years start at 2007
-    // the countries are labeled 0 through 5
-    var baseYear = 2007
+    // declare global year and country list for user selection
+    // create separate lists since the countries in the
+    // datasets are formatted differently
 
-    var values = []
+    // the datasets are formatted differently, so check for proper formatting
+    if (data.structure.dimensions.series.length === 2) {
+      format = 1 //women
+      set = 0
+      window.countries1 = [];
+    } else {
+      format = 0 //cons
+      set = 1
+      window.countries2 = [];
+    }
+
+    // retrieve the countries from the dataset
+    countryIndex = Object.keys(data.structure.dimensions.series[format].values)
+    countryIndex.forEach(function(i) {
+      if (format === 1) {
+          countries1.push(data.structure.dimensions.series[format].values[i].name)
+      } else {
+          countries2.push(data.structure.dimensions.series[format].values[i].name)
+      }
+    })
+    window.years = [];
+
+    // retrieve the years from the dataset
+    yearData = Object.keys(data.structure.dimensions.observation[0].values)
+    yearData.forEach(function(i) {
+      years.push(data.structure.dimensions.observation[0].values[i].id)
+    })
+
+    var values = [];
 
     // extract the object of country objects
-    let countryData = Object.keys(data["dataSets"][0]["series"])
+    let countryData = Object.keys(data.dataSets[0].series)
 
     // loop over each county's observations and create array of data objects
     countryData.forEach(function(i) {
-        let obs = Object.keys(data["dataSets"][0]["series"][i]["observations"]);
+        let obs = Object.keys(data.dataSets[0].series[i].observations);
 
         // the country 'number' is differently formatted in each dataset
         if (i.length === 3) {
-            country = countries[i[2]]
+            country = countries1[i[2]]
         } else {
-            country = countries[i[0]]
+            country = countries2[i[0]]
         }
 
-        // create datapoints and add them to the values list
+        // create emty country datalist
+        let countryYears = [];
+        // create datapoints and add them to the values list //j is year index
         obs.forEach(function(j) {
-            let year = (Number(j) + baseYear);
-            let value = data["dataSets"][0]["series"][i]["observations"][j][0];
-            let dataPoint = {country:country, year:year, value:value};
-            values.push(dataPoint);
-      })
+            let year = years[j];
+            let value = data.dataSets[0].series[i].observations[j][0];
+            countryYears.push({[years[j]]:value})
+        })
+        values.push({[country]:countryYears})
     })
     return values
 }
@@ -133,8 +193,8 @@ function makeDots(set1, set2) {
   // this way there no missing data
   dotList = [];
   for (i in set1) {
-      dotList.push({country:set1[i].country, year:set1[i].year,
-                    women:set1[i].value, cons:set2[i].value});
+      year = Object.keys(set1[i])[0]
+      dotList.push({[year]:[set1[i][year], set2[i][year]]})
   }
   return dotList
 }
@@ -143,7 +203,7 @@ function calc(set, stat) {
     // calculate a min or max from an array of objects
     stats = [];
     for (i in set) {
-      stats.push(set[i].value)
+      stats.push(set[i][Object.keys(set[i])])
     }
     if(stat === "max") {
         return Math.max.apply(null, stats)
